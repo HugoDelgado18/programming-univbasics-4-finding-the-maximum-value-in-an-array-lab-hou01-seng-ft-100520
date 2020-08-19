01dce22145b5fee5307af7b5b16da3b1081e7bc3
Hugo Delgado <hugo.delgado1@outlook.com>
2020-08-19 04:31:35 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -3,7 +3,7 @@ def find_max_value(array)
   counter = 0
 
   while counter < array.length do
-    if array[counter] == array.max
+    if array[counter] == array.last
       return counter
     end
     counter += 1
